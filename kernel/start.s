442961f231f5ea82d78e32d3716b1287291a6a27
Niccolò Pratesi <n.pratesi1@studenti.unipi.it>
2024-08-07 14:32:37 +0200

    Modificato il file kernel/start.s
    Modificata la gestione delle interruzioni
    - disabilitate le interruzioni per il livello corrente azzerando i campi
      MIE e SIE del registro mstatus
    - disabilitate tutte le interruzioni a livello macchina azzerando il
      registro mie
    
    Modificata l'inizializzazione del timer:
    - abilitata l'estensione Sstc che permette di utilizzare i csr stime e
      stimecmp, equivalenti di mtime e mtimecmp per il livello supervisore
    - il timer emulato da QEMU aggiorna il contatore ogni 0.1us per cui il
      valore di TIMER_DELAY e' stato impostato a 500000, dato che vogliamo
      le interuzioni ogni 50ms
    - adesso il valore del contatore viene letto dal csr time (prima era il
      registro in memoria mtime)
    - adesso il tempo del prossimo interrupt del timer da inviare viene
      salvato nel csr stimecmp (prima era il registro in memoria mtimecmp)
    - abilitati i csr stime e stimecmp per essere visibili in modalità
      supervisore

--- kernel/start.s
+++ kernel/start.s
@@ -1,5 +1,5 @@
-.globl timer_machine_handler
-.equ TIMER_DELAY, 10000000
+.global k_trap
+.equ TIMER_DELAY, 500
 .global start
 start:
   # set M Previous mode to Supervisor for mret
@@ -34,35 +34,34 @@ start:
   # Init timer #####
   
   # Schedule the first interrupt
-  li a0, 0x200BFF8    # MTIME mem location
-  ld a1, 0(a0)        # Load MTIME, which are cycles since boot
-  li a2, TIMER_DELAY  # The delay at which to fire the int
-  add a2, a2, a1      # Get next cycle at which to fire the int
-  li a0, 0x2004000    # MTIMECMP mem location
-  sd a2, 0(a0)
+  csrr t0, time       # Load time csr, which are cycles since boot
+  li t1, TIMER_DELAY  # The delay at which to fire the int
+  add t1, t1, t0      # Get next cycle at which to fire the int
+  csrw stimecmp, t1   # Save the next cycle into the stimecmp csr
 
-  # Set timer_machine_handler as the machine interrupt handler
-  la t0, timer_machine_handler
-  csrw mtvec, t0
+  # Disable current privilege interrupts
+  li t0, 0b1010
+  csrc mstatus, t0
 
-  # Enable machine mode interrupts
-  li t0, 8
-  csrs mstatus, t0
-
-  # Enable timer interrupts
-  li t0, 128
+  # Disable machine mode interrupts
+  li t0, 0
   csrw mie, t0
+
+  # Enable Sstc extension
+  # li t0, 0x8000000000000000
+  # csrs menvcfg, t0
+
+  # Enable time and stimecmp CSRs to be visibe in supervisor mode
+  li t0, 0b10
+  csrs mcounteren, t0
 
   # Set k_trap as Supervisor interrupt handler
   la t0, k_trap
   csrw stvec, t0
 
   # Enable Supervisor Mode Interrupts
-  csrr a0, sie
-  ori a0, a0, 0x200
-  ori a0, a0, 0x20
-  ori a0, a0, 0x2
-  csrw sie, a0
+  li t0, 0b1000100010
+  csrs sie, t0
 
   # Enable memory access at S-mode when BIT_U=1 
   # Set the SUM bit (18) in sstatus 
