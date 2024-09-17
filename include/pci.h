df66f1e03b82f35ed89b4f9c274bb4281b1cc406
Niccolò Pratesi <n.pratesi1@studenti.unipi.it>
2024-09-17 17:45:57 +0200

    Aggiunta la definizione della struttura PCI_config
    Aggiunta la definizione della struttura PCI_config utilizzata per
    accedere con più semplicità ai campi della parte di configurazione delle
    periferiche PCI che sono mappate in memoria.

--- include/pci.h
+++ include/pci.h
@@ -6,3 +6,35 @@ namespace pci {
 }
 
 natl* make_CAP(natb bus, natb dev, natb fun, natb off);
+
+struct PCI_config {
+  natw vendor_id;
+  natw device_id;
+  natw command;
+  natw status;
+  natb revision_id;
+  natb prog_if;
+  natb subclass;
+  natb class_code;
+  natb cache_line_size;   
+  natb latency_timer;
+  natb header_type;
+  natb bist;
+  natl bar0;
+  natl bar1;
+  natl bar2;
+  natl bar3;
+  natl bar4; 
+  natl bar5;  
+  natl cardbus_cis_pointer;
+  natw subsystem_vendor_id;
+  natw subsystem_id;
+  natl expansion_rom_base_address;
+  natb capabilities_pointer;
+  natb padding[7];
+  natb interrupt_line;
+  natb interrupt_pin;
+  natb min_grant;
+  natb max_latency;
+};
+
